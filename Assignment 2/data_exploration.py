9d27b1e2c07a0860f173d9d468f038e6b46dd37b
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-03-23 00:12:04 +1100

    Separate attributes into groups for later modular implementation

--- Assignment 2/data_exploration.py
+++ Assignment 2/data_exploration.py
@@ -11,3 +11,8 @@ print("\nData Types:\n", df.dtypes)
 print("\nMissing Values:\n", df.isnull().sum())
 print("\nDescriptive Statistics:\n", df.describe())
 
+#Grouping each column by attribute type
+nominal_cols = ['gender', 'employment_status', 'loan_purpose', 'region', 'marital_status', 'loan_default']
+ordinal_cols = ['education_level']
+interval_cols = ['credit_score', 'application_year', 'application_date']
+ratio_cols = ['age', 'income', 'loan_amount', 'debt_to_income_pct', 'num_dependents', 'years_employed', 'account_balance', 'num_prev_loans', 'monthly_expenses', 'loan_duration_months']
